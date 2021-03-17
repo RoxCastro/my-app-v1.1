afb8d3533b16092d045bb86d1be47d54b5b93242
ROSARIO CASTRO <rosario_castro_c@outlook.com>
2021-03-16 19:47:32 -0600

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Contador from './componentes/Contador';
+//import Contador from './componentes/componente1';
+
+function App() {
+  return (
+    <div>
+<h3>Cliente: Cooperativa del Pueblo S.A. de C.V.</h3>
+<h3> Nombre del proyecto: Implementación de software para la gestión de asistencia y puntualidad</h3>
+<center><h2>Sprint Planning</h2></center>
+
+<Contador/>
+<componente1/>
+  
+      </div>
+  );
+}
+
+export default App;
