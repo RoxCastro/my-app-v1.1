c76bde3ef410e68808cb9d4af3dbcfff252cbe42
RoxCastro <80506678+RoxCastro@users.noreply.github.com>
2021-03-17 21:52:41 -0600

    Add files via upload

---
+++ HeadTabla.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const HeadTabla = () => {
+  return (
+    <thead>
+      <tr>
+      <th> Fecha de inicio   </th>
+        <ul></ul>
+        <th> Fecha de fin   </th>
+        <ul></ul>
+        <th> Fechas de revisión </th>
+        <ul></ul>
+        <th>Tareas a realizar </th> 
+      <ul></ul>
+      </tr>
+    </thead>
+  );
+}
+  
+export default HeadTabla;
---
+++ HeadTabla1.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const HeadTabla1 = () => {
+  return (
+    <thead>
+      <tr>
+        <th> Fecha de inicio   </th>
+        <ul></ul>
+        <th> Fecha de fin   </th>
+        <ul></ul>
+        <th> Fechas de revisión </th>
+        <ul></ul>
+        <th>Tareas a realizar </th> 
+      <ul></ul>
+ 
+      </tr>
+    </thead>
+  );
+}
+  
+export default HeadTabla1;
